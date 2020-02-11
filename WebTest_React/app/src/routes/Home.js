7931fb1a2257e7dddbfb9de6d16c782beb67a0ef
Min <ken1336@naver.com>
2020-02-11 09:49:54 +0900

    1

--- WebTest_React/app/src/routes/Home.js
+++ WebTest_React/app/src/routes/Home.js
@@ -1,16 +1,17 @@
-import React, {Component, Fragment} from 'react';
+import React, { Component, Fragment } from "react";
 
-
-
-
-class Home extends Component{
-
-    render() {
-        return (
-            "Hello! This is min's project"
-
-        );
-    }
+export default function Home() {
+    const a = () => 0 - Math.random(Date.now())
+    const array = Array
+    .apply(0,Array(45))
+    .map((v,i) =>i+1)
+    .sort(a)
+    //.slice(0,6);
+    console.log(array);
+    const showArray = array.map(v=><span>{v+' '}</span>)
+    
+    console.log(a())
+    console.log(a())
+    console.log(a())
+  return <div>{}</div>;
 }
-
-export default Home;
