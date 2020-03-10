513d62aeebb515a90dd8e65b04dc65b3012c36b5
김형민 <ken1336@naver.com>
2020-03-10 22:59:18 +0900

    Added Calendar UI +1

--- WebTest_React/app/src/views/TestComponent.js
+++ WebTest_React/app/src/views/TestComponent.js
@@ -1,22 +1,28 @@
-import React, {Component, Fragment} from 'react';
-import MinSwitch from '../components/MinSwitch';
-import MinButton from '../components/MinButton'
-import MinTable from '../components/MinTable';
+import React from "react";
 
+import FullCalendar from "@fullcalendar/react";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import interactionPlugin from '@fullcalendar/interaction';
+import "@fullcalendar/core/main.css";
+import "@fullcalendar/daygrid/main.css";
 
+export default function TestComponent() {
+  const events = [{ title: "today's event", date: new Date() },{title: "today's event2", start  : '2020-03-05',
+  end    : '2020-03-07'}];
 
-class Home extends Component{
-
-    render() {
-        return (
-            <Fragment>
-                <MinSwitch/>
-                <MinButton/>
-               
-            </Fragment>
-
-        );
-    }
+  const testEvent = () => {
+      alert("test");
+  }
+  return (
+    <div className="App" style={{padding:"100px"}}>
+      <FullCalendar
+        defaultView="dayGridMonth"
+        plugins={[dayGridPlugin,interactionPlugin]}
+        events={events}
+        eventClick = {testEvent}
+        editable = "true"
+        droppable = "true"
+      />
+    </div>
+  );
 }
-
-export default Home;
